0e26f66216e6aedf5373c6fb8bd6eb0a36a7106b
rodrigolima131 <rodrigo.lima131@gmail.com>
2019-07-02 12:06:11 -0300

    criado classes departament,workerlevel,contract e work

---
+++ Contrato/Entites/Enums/WorkerLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contrato.Entites.Enums
+{
+    enum WorkerLevel : int
+    {
+    }
+}
